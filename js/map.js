7ef3a70acc6a7effe435617d6ef265712821d816
yohman <yohman@gmail.com>
2020-03-14 10:25:15 -0700

    added circle color to show percent change

--- js/map.js
+++ js/map.js
@@ -17,6 +17,8 @@
 	corona.data = {};
 	corona.scale = 'log' // log | proportional
 	corona.data_label = 'confirmed cases'
+	corona.circles = [] //placeholder for the circles
+	corona.currentDate = ''
 
 /***
 
@@ -27,12 +29,6 @@
 $(document).ready(function() {
 	corona.getData();
 });
-
-corona.changeDataLabel = function(label)
-{
-	corona.data_label = label
-	corona.getData()
-}
 
 corona.getData = function()
 {
@@ -107,84 +103,68 @@ corona.getData = function()
 }
 
 
+/***
+
+	Map Settings
+
+***/
 corona.setParameters = function()
 {
 
-	/***
+	// instantiate the map
+	corona.map = L.map('map',{
+		zoomControl: false,
+	});
 
-		Map Settings
+	//add zoom control with your options
+	L.control.zoom({
+	     position:'topright'
+	}).addTo(corona.map);
 
-	***/
+	// info control
+	corona.info = L.control();
 
-		// instantiate the map
-		corona.map = L.map('map',{
-			zoomControl: false,
-		});
+	corona.basemapmapoptions = {
+		maxZoom: 		20,
+		tileSize: 		512,
+		zoomOffset: 	-1,
+		// retina: '@2x',
+		// detectRetina: true,
+		attribution: 	'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+	}
 
-		//add zoom control with your options
-		L.control.zoom({
-		     position:'topright'
-		}).addTo(corona.map);
+	corona.basemaps = [
+		L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', corona.basemapmapoptions),
+		L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', corona.basemapmapoptions),
+		L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', corona.basemapmapoptions),
+	]
 
-		// info control
-		corona.info = L.control();
-
-		// the geography layer
-		// corona.mapLayer = L.geoJson();
-
-		corona.basemapmapoptions = {
-			maxZoom: 		20,
-			tileSize: 		512,
-			zoomOffset: 	-1,
-			// retina: '@2x',
-			// detectRetina: true,
-			attribution: 	'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-				'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-				'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-		}
-
-		corona.basemaps = [
-			L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', corona.basemapmapoptions),
-			L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', corona.basemapmapoptions),
-			L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', corona.basemapmapoptions),
-		]
-
-
-		// the basemap
-		corona.basemap = corona.basemaps[1] //0: light 1: dark 2: satellite
+	// the basemap default
+	corona.basemap = corona.basemaps[1] //0: light 1: dark 2: satellite
 
 	corona.init()
 }
 
+/***
+
+	Initialize the Map
+
+***/
 corona.init = function()
 {
-	/*
-
-		Initialize the Map
-
-	*/
 	console.log('initializing...')
 
-	corona.map.setView([0,0], 2);
+	corona.map.setView([20,30], 2);
 	corona.map.addLayer(corona.basemap); 
 	// corona.displayLegend();
-
-	// add circles around F1 (10km, 20km, 30km)
-	var circleStyle = {
-		"color": "white",
-		"fillColor": "red",
-		"weight": 1,
-		"opacity": 0.8,
-		"fillOpacity": 0.5
-	};
-	// L.circle([37.422116, 141.030259], 10000,circleStyle).addTo(corona.map);
-	// L.circle([37.422116, 141.030259], 20000,circleStyle).addTo(corona.map);
-	// L.circle([37.422116, 141.030259], 30000,circleStyle).addTo(corona.map);
-
 
 	// add the timebar
 	corona.setTimebar();
 	timebar.update({from:0})
+
+	// start the animation by default
 	corona.animate()
 
 	// add basemap toggles
@@ -198,57 +178,55 @@ corona.init = function()
 		corona.changeBaseMap(2)
 	})
 
-
-
 }
 
+/***
+
+	Animate the Map
+
+***/
 corona.animate = function()
 {
 	console.log('animating...')
-	// disable buttons
+
+	// disable buttons while animating
 	$('#btn-confirmed').prop('disabled',true)
 	$('#btn-deaths').prop('disabled',true)
-	// $('#btn-confirmed').addClass('disabled')
-	// $('#btn-deaths').addClass('disabled')
 
 	time = 200
+	// loop through each date, wait, and then go to the next one
 	$.each(corona.data.covid_time_series.headers,function(i,val){
 		var t = setTimeout(function(){
 			timebar.update({from:i})
 		},time*i)
-			// console.log(i)
 
+		// when you've reached the last date, enable the buttons
 		if(corona.data.covid_time_series.headers.length-1 == i)
 		{
-			console.log('inside...')
 			var t = setTimeout(function(){
 				$('#btn-confirmed').prop('disabled',false)
 				$('#btn-deaths').prop('disabled',false)
-				// $('#btn-confirmed').removeClass('disabled')
-				// $('#btn-deaths').removeClass('disabled')
 			},time*i)
 		}
-		// time = time + 1000
-
 	})
-
-    // $("div").each(function(index) {        
-    //     var that = this;
-    //     var t = setTimeout(function() { 
-    //         $(that).removeClass("invisible"); 
-    //     }, 500 * index);        
-    // });
-
 
 }
 
+// change map to show confirmed cases or deaths
+corona.changeDataLabel = function(label)
+{
+	corona.data_label = label
+	corona.getData()
+}
+
+// find the max for any day in the data
 function getMaxData()
 {
-
 	var maxdata = 0
 	// find max num in all of the data
 	$.each(corona.data.covid_time_series.data,function(i,val){
 		for (var i = val.length - 1; i >= 0; i--) {
+			// first 4 columns are not data values
 			if(i>3)
 			{
 				if(parseInt(val[i])>maxdata)
@@ -262,6 +240,7 @@ function getMaxData()
 	return maxdata
 }
 
+// find the total for any given day
 function getTotalByDate(date)
 {
 	var maxdata = 0
@@ -277,14 +256,12 @@ function getTotalByDate(date)
 	return maxdata
 }
 
+// this determines the size of the circles
 function getProportionalCircleSize(num)
 {
 
-	// if(num<minsize){
-	// 	num = minsize
-	// }
-		const maxsize = 100
-		const minsize = 3
+	const maxsize = 100
+	const minsize = 3
 
 	if(corona.scale == 'proportional')
 	{
@@ -301,6 +278,12 @@ function getProportionalCircleSize(num)
 			circlesize = minsize
 		}
 	}
+	/***
+		
+		logarithmic numbers allows outliers,
+		in this case China, not to dominate the map
+
+	***/
 	else if (corona.scale == 'log')
 	{
 		circlesize = Math.log2(num)*2
@@ -309,25 +292,45 @@ function getProportionalCircleSize(num)
 			circlesize = minsize
 		}
 	}
-	// const maxsize = 100
-	// const max = 10000
-	// if(num>max){
-	// 	num = max
-	// }
-	// circlesize = num*maxsize/max
-	// circlesize = num*maxsize/max
+
 	return circlesize
 }
 
 
-/*
+/***
 
 	Map the data
 
-*/
-corona.circles = []
-corona.currentDate = ''
+***/
 
+corona.getPreviousDataValue = function(pos)
+{
+	// find where in the array the current date is
+	const cur = corona.data.covid_time_series.headers.indexOf(corona.currentDate)
+	if (cur > 0)
+	{
+		const prev = cur -1
+		const prev_date = corona.data.covid_time_series.headers[prev]
+		return corona.data.covid_time_series[prev_date][pos]
+	}
+	else
+	{
+		return false
+	}
+}
+
+corona.getSparklineData = function(pos)
+{
+	const placedata = corona.data.covid_time_series.data[pos+1]
+	const sparkdata = []
+	$.each(placedata,function(i,val){
+		if(i>3)
+		{
+			sparkdata.push(val)
+		}
+	})
+	return sparkdata
+}
 
 corona.mapCoronaData = function(date)
 {
@@ -347,19 +350,57 @@ corona.mapCoronaData = function(date)
 		// only map if it is a country
 		if(val[4]>0)
 		{
+
+			prev_day_record = corona.getPreviousDataValue(i);
+			prev_day_value = prev_day_record[4]
+			cur_day_value = val[4]
+			percent_increase = Math.round((cur_day_value- prev_day_value)/prev_day_value*100)
+
+			if(percent_increase>50)
+			{
+				fillColor = '#de2d26'
+			}
+			else if (percent_increase>20)
+			{
+				fillColor = '#fc9272'
+			}
+			else
+			{
+				fillColor = '#fee0d2'				
+			}
+
 			var circleStyle = {
 				"radius": getProportionalCircleSize(val[4]),
 				"color": "white",
-				"fillColor": "red",
+				"fillColor": fillColor,
 				"weight": 1,
-				"opacity": 0.8,
+				"opacity": 0.5,
 				"fillOpacity": 0.2
 			};
 
 			corona.circles[i] = L.circleMarker([val[2], val[3]], circleStyle).addTo(corona.map);			
-			corona.circles[i].bindPopup('<h2>'+val[4]+' '+corona.data_label+'</h2>'+val[0]+' '+val[1])
+			corona.circles[i].bindPopup('<h2 style="text-align:center;font-size:3em;">'+val[4]+'</h2><p style="text-align:center;padding:0px;margin:0px">Previous day: '+prev_day_value +' ('+percent_increase+'% increase)'+'</p><p style="text-align:center;font-size:1em;padding:0px;margin:0px">'+corona.data_label+' in '+val[0]+' '+val[1]+'</p>')
+
+
+
+
 			corona.circles[i].on('mouseover',function(e){
+
+				console.log(e)
+				// content = $('#spark').sparkline(corona.getSparklineData(i), {
+				// 	type: 'bar'
+				// });
+
+
+				// var popup = e.target.getPopup();
+				// // var chart_div = document.getElementById("graphdiv");
+				// popup.setContent( content );
 				this.openPopup()
+
+
+				// $.sparkline_display_visible();
+
+
 			})
 			corona.circles[i].on('mouseout',function(e){
 				this.closePopup()
@@ -370,11 +411,11 @@ corona.mapCoronaData = function(date)
 
 }
 
-/*
+/***
 
 	Add the timebar
 
-*/
+***/
 var timebar = ''
 corona.setTimebar = function()
 {
